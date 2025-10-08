7a39f0f70b61f0a3867ed49685dda8b5528d3a29
Miguel Vieira <152816803+MiguelOlivieira@users.noreply.github.com>
2025-10-08 18:12:33 -0300

    feat: package models and Class User added

---
+++ models/user.py
@@ -0,0 +1,15 @@
+from database import db
+from flask_login import UserMixin
+
+                    #Herda metodos do userMixin
+class User(db.Model, UserMixin): #Model que faz parte do proprio SQLAlchemy
+    #id(int), username(text), password(text)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True) #Coluna obrigatoria e unica
+    password = db.Column(db.String(80), nullable=False)
+    
+    
+    #flask shell
+    #db.create_all()
+    #db.session.commit()
+    #exit()
